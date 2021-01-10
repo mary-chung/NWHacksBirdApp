eb8e7b02137b9d150c75d7d3ddee35fc76942bca
jun <jun585jun@gmail.com>
2021-01-10 00:13:15 -0800

    worked on reading from sql files

---
+++ app/src/main/res/raw/birds.sql
@@ -0,0 +1,32 @@
+INSERT INTO Birds (cname, sname, size, funfact, habitat, sizerange, diet, appearance)
+VALUES ('Song sparrow', 'Melospiza melodia', 'S', 'Male song sparrows sing to attract mates and defend its territory.', 'Marsh edges, fields, yards, forest edges, residential areas', '12-17 cm', 'Insects, seeds, fruits, grains', 'small round bodies, brown with thick streaks on a white chest, red-brown and grey head');
+
+INSERT INTO Birds (cname, sname, size, funfact, habitat, sizerange, diet, appearance)
+VALUES ('Mallard', 'Anas platyrhynchos', 'M', 'Male mallards don''t quack!', 'Marshes, ponds, lakes, parks', '50-65 cm', 'Seeds, plants, worms, snails', 'rounded head, flat bill, males have a green head and yellow bill, females are brown with a brown bill');
+
+INSERT INTO Birds (cname, sname, size, funfact, habitat, sizerange, diet, appearance)
+VALUES ('Mew gull', 'Larus canus', 'M', 'Mew gulls are the only white-headed gulls that use trees for nesting.', 'Tundra, marshes, ponds, lakes, shores, beaches, islands', '41-46 cm', 'Fish, insects, earthworms, grains, marine invertebrates - almost everything!', 'Adults have a yellow bill, white head, grey back, black wingtips, yellow legs');
+
+INSERT INTO Birds (cname, sname, size, funfact, habitat, sizerange, diet, appearance)
+VALUES ('Black-capped chickadee', 'Poecile atricapillus', 'S', 'Black-capped chickadees have incredibly good memory! They hide food in thousands of hiding places to snack on later.', 'Trees, forests, parks, marshes', '12-15 cm', 'Seeds, berries, insects', 'Oversized head, small body, white cheeks, gray back, ray wings, gray tail, white underside');
+
+INSERT INTO Birds (cname, sname, size, funfact, habitat, sizerange, diet, appearance)
+VALUES ('Northwestern crow', 'Corvus caurinus', 'M', 'Northwestern crows are able to remember where they hid clams, even after the tide has come and gone!', 'Coastal areas with sparse trees, residential, industrial, agricultral areas, parks', '42-45 cm', 'Fruit, seeds, eggs, clams, crabs, earthworms, insects - almost everything!', 'All black, long, broad bill and wings, heavy legs and feet');
+
+INSERT INTO Birds (cname, sname, size, funfact, habitat, sizerange, diet, appearance)
+VALUES ('Blue jay', 'Cyanocitta cristata', 'S', 'Blue jays love acorns!', 'Mixed wood forests, residential areas', '22-30 cm', 'Seeds, grains, berries, fruits', 'Blue crest, blue back, grey underside, black and white markings on wings and tail, black bill');
+
+INSERT INTO Birds (cname, sname, size, funfact, habitat, sizerange, diet, appearance)
+VALUES ('Glaucous-winged gull', 'Larus glaucescens', 'M', 'Glaucous-winged gulls have very broad diets. They eat everything! Don''t let them in a buffet.', 'Marine shorelines, rivermouths, bays, estuaries', '50-59 cm', 'Fish, insects, birds, berries', 'Adults: grey back and wingtips, white underside, yellow bill, pink feet and legs. Juveniles: pale brown overall');
+
+INSERT INTO Birds (cname, sname, size, funfact, habitat, sizerange, diet, appearance)
+VALUES ('Great blue heron', 'Ardea herodias', 'L', 'Great blue herons have great night vision!', 'marsh, river, ponds', '97-137 cm', 'fish', 'blue-grey colour, long necks and thick bill');
+
+INSERT INTO Birds (cname, sname, size, funfact, habitat, sizerange, diet, appearance)
+VALUES ('Double-crested cormorant', 'Phalacrocorax auritus', 'L', 'The inside of Double-crested cormorants'' mouths are bright blue!', 'lakes and coasts', '70-90 cm', 'fish', 'large body but small heads, brown-black colour');
+
+INSERT INTO Birds (cname, sname, size, funfact, habitat, sizerange, diet, appearance)
+VALUES ('Anna’s hummingbird', 'Calypte anna', 'S', 'How do you describe a group of hummingbirds? Use beautiful adjectives like a "bouquet, glittering, or shimmer" of hummingbirds!', 'yards, parks', '10 cm', 'nectar, insects, sugar-water mixtures in feeders', 'Male: red head + throat and green + grey body Female: green + grey body');
+
+INSERT INTO Birds (cname, sname, size, funfact, habitat, sizerange, diet, appearance)
+VALUES ('Harlequin duck', 'Histrionicus histrionicus', 'L', 'Harlequin ducks squeak to other ducks!', 'rivers', '33-46 cm', 'insects', 'Male: blue with white stripes, reddish-brown sides Female: Grey-brown colour');
